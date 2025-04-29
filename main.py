94d6c62b2217fc7c326c4bd4806aabdbcdec46e9
waseem2803 <mwas2803@gmail.com>
2025-04-30 00:06:13 +0530

    camera interfaced

--- main.py
+++ main.py
@@ -7,37 +7,25 @@ from threading import Thread
 import time
 import requests
 
+ESP32_CAM_IP = "http://192.168.0.5/capture"  # Replace <ESP32_IP> with your ESP32 IP
+
 app = Flask(__name__)
 
 def detect(file):
     model = YOLO("best.pt")
-    results = model.predict(file, conf=0.9)
+    results = model.predict(file, conf=0.7)
     result = results[0]
     try:
         len(result.boxes)
         box = result.boxes[0]
         class_id = result.names[box.cls[0].item()]
         currency = class_id 
+        print("currency:", currency)
     except:
         print("No object detected")
         currency = 0
     return currency
 
-def fetch_image():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    async def fetch():
-        protocol = await aiocoap.Context.create_client_context()
-        request = aiocoap.Message(code=aiocoap.GET, uri="coap://device_ip/get_image")
-        try:
-            response = await protocol.request(request).response
-            with open("received_image.jpg", "wb") as f:
-                f.write(response.payload)
-        except Exception as e:
-            print(f"Error fetching image: {e}")
-
-    loop.run_until_complete(fetch())
 
 #initializing the camera page
 @app.route('/', methods = ['GET'])
@@ -47,21 +35,23 @@ def index():
 
 def get_image():
     try:
-        url = "http://device_ip/get_image"
-        response = requests.get(url, stream=True)
+        print(f"Requesting image from {ESP32_CAM_IP}...")
+        response = requests.get(ESP32_CAM_IP, timeout=10)
+
         if response.status_code == 200:
-            with open("received_image.jpg", "wb") as f:
-                for chunk in response.iter_content(1024):
-                    f.write(chunk)
+            with open('received_image.jpg', 'wb') as f:
+                f.write(response.content)
+            print("Image saved as 'received_image.jpg'")
         else:
-            print(f"Failed to fetch image. Status code: {response.status_code}")
+            print(f"Failed to get image. Status code: {response.status_code}")
+
     except Exception as e:
-        print(f"Error fetching image: {e}")
+        print(f"An error occurred: {e}")
 
 @app.route('/detect', methods = ['POST' ,'GET'])
 def detect_currency(): 
 
-    thread = Thread(target=fetch_image)
+    thread = Thread(target=get_image)
     thread.start()
     thread.join()
     image = "received_image.jpg"
